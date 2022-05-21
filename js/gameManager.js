ee68b43bf9f4c0514987ec2dba18047e1165687c
Gabriel Fidelles <gabriel.fidelles@lumis.com.br>
2022-05-21 19:18:32 -0300

    Commiting formulas of damage calcule

--- js/gameManager.js
+++ js/gameManager.js
@@ -8,3 +8,25 @@ async function startNewGame(trainerPkmnName, enemyPkmnName){
     console.log(trainerPkmn);
     console.log(enemyPkmn);
 }
+
+function attack(){
+
+}
+
+/*
+
+    SPECIAL ATK: ((((2*level/5+2)*spatkstat*atkpwr/spdefstat)/50)+2)*stab*weakness;
+    SPECIAL DEF: resistance*critical*other(margin/100)
+
+    PHYSICAL ATK: ((((2*level/5+2)*spatkstat*atkpwr/spdefstat)/50)+2)*stab*weakness;
+    PHYSICAL DEF: 
+
+*/
+
+function getPokeStatusValueByName(pokeObj, statusName){
+    pokeObj.stats.filter((stat) => {
+        if(stat.stat.name == statusName){
+            return stat;
+        }
+    })
+}
